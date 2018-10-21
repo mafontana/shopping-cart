b72b5d207a90ebfb52c4fd019df80991805ffa27
Meredith Fontana <meredith@Merediths-MacBook-Pro.local>
2018-10-21 15:04:28 -0600

    fixed formatting of columns

--- src/components/CartItemComponent.js
+++ src/components/CartItemComponent.js
@@ -5,14 +5,19 @@ class CartItemComponent extends Component {
     render () {
         console.log(this.props)
         return (
-            <div className="collection-item">
             
-            <div className="row">
-              <div className="col-md-8">{this.props.cartItemName}</div>
-              <div className="col-md-2">{this.props.cartItemPrice}</div>
-              <div className="col-md-2">{this.props.cartItemQuantity}</div>
+            <div className="list-group">
+                    <div className="list-group-item">
+                            <div className="row">
+                                <div className="col-md-8">{this.props.cartItemName}</div>
+                                <div className="col-md-2">{this.props.cartItemPrice}</div>
+                                <div className="col-md-2">{this.props.cartItemQuantity}</div>
+                            </div>
+                    
+            
             </div>
-          </div>
+            </div>
+            
         )
     }
 
